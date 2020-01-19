0a7c964d2c10749754a3803a2777c77c380dd0fe
Mariano Conti <nanexcool@gmail.com>
2020-01-19 16:41:21 -0300

    Updates

---
+++ precache-manifest.af690efd151bd3cd284f6f839ddd3473.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e139ed5021c0ff984466d6e9ae2fab",
+    "url": "/aztec-airdrop/index.html"
+  },
+  {
+    "revision": "63e7b2ae07cc92ed767a",
+    "url": "/aztec-airdrop/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "f8d95f441334ae3e2d85",
+    "url": "/aztec-airdrop/static/js/2.905d95e5.chunk.js"
+  },
+  {
+    "revision": "446737d912f911e17f63aeea465f18e1",
+    "url": "/aztec-airdrop/static/js/2.905d95e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63e7b2ae07cc92ed767a",
+    "url": "/aztec-airdrop/static/js/main.cca1c763.chunk.js"
+  },
+  {
+    "revision": "13f9a2531fa297ea651e",
+    "url": "/aztec-airdrop/static/js/runtime-main.106e1cd0.js"
+  }
+]);
